609da696bbcc6cf378ae9a129c7b2eeacb16a77c
powerar <arpower@bell.net>
2021-05-22 13:08:18 -0400

    added database columns

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,20 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
