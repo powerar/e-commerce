a398bb26ae71f6158c81a97367457afd066af5bf
powerar <arpower@bell.net>
2021-05-23 11:16:13 -0400

    validate product data

--- models/Product.js
+++ models/Product.js
@@ -17,18 +17,19 @@ Product.init(
     },
     product_name: {
       type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
+      allowNull: false
     },
     price: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.DECIMAL,
       allowNull: false,
     },
     stock: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      defualtValue: 10,
+      validate: {
+        isAlphanumeric: true
+      }
     },
     category_id: {
       type: DataTypes.INTEGER,
